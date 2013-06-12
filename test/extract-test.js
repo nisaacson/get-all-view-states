ea359da4d78c4890b3410c0c12e9ef6c293b1d4e
Noah Isaacson <noah@nisaacson.com>
2013-06-12 11:44:14 -0600

    added previous page test

--- test/extract-test.js
+++ test/extract-test.js
@@ -1,3 +1,5 @@
+
+var inspect = require('eyespect').inspector({maxLength: 99999})
 var should = require('should')
 var fs = require('fs')
 var path = require('path')
@@ -5,13 +7,15 @@ var cheerio = require('cheerio')
 var getAllViewStates = require('../')
 describe('Extract View State', function () {
   it('should extract data', function () {
-    var desiredViewStateCount = '15'
+    var desiredViewStateCount = '12'
     var html = fs.readFileSync(path.join(__dirname, 'data/sample.html'),'utf8')
     var $ = cheerio.load(html)
     var viewStates = getAllViewStates($)
     should.exist(viewStates)
     should.exist(viewStates['__VIEWSTATEFIELDCOUNT'])
     should.exist(viewStates['__VIEWSTATE'])
+    should.exist(viewStates['__PREVIOUSPAGE'])
+    inspect(viewStates,'viewStates')
     var count = viewStates['__VIEWSTATEFIELDCOUNT']
     count.should.eql(desiredViewStateCount)
   })
